7e84e8c04ca48850634b79c383a796c02e11c27d
evertcolombia <evertescalante@gmail.com>
2019-12-16 17:02:21 -0500

    4-guess the number script

---
+++ 0x05-functions/4-guess_the_number.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# This is a guess the number game
+
+import random
+secret_number = random.randint(1, 20)
+print("I'm thinking of a number between 1 and 20")
+
+# Ask the player to guess 6 times
+for guessesTaken in range(1, 7):
+    print('Take a guess.')
+    guess = int(input())
+
+    #look for the answer
+    if guess < secret_number:
+        print('Your guess is too low')
+    elif guess > secret_number:
+        print('Your guess is too hight')
+    else:
+        break #this condition is the correct
+    
+if guess == secret_number:
+    print('Good job! You guessed my number in' + str(guessesTaken) + 'guesses!')
+else:
+    print('Nope. the number I was thinking of was ' + str(secret_number))
