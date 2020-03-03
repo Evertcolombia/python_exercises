ff973666c684b6cfe19d86fd42f16c261b4c53b9
evertcolombia <evertescalante@gmail.com>
2020-03-03 00:03:40 -0500

    hotfix

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,3 @@
+-- utf8 encoding for database and table
+ALTER DATABASE CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
