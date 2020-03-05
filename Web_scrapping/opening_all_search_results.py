e90715b8bb427c38534eaee810500e45fb1de89b
evertcolombia <evertescalante@gmail.com>
2020-03-05 18:07:11 -0500

    hotfix

--- Web_scrapping/opening_all_search_results.py
+++ Web_scrapping/opening_all_search_results.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 import sys, requests, webbrowser, bs4
+"""Simple script to open the first 5th web pages that contains
+    the refered user looks
+"""
 
-def simple_request(browser_url):
+def simple_request():
+    """ 
+        Do simple request based in the search string
+    """
     print('Googling...')
     browser_url = 'https://google.com/search?q='
     res = requests.get(browser_url + ''.join(search_string))
@@ -10,6 +16,11 @@ def simple_request(browser_url):
 
 
 def link_elements(res):
+    """
+        get the elements html <a> with the refer url por the pages
+
+        [0] res: response of the request
+    """
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
     linkElems = soup.select('.ZINbbc>div.kCrYT a')
 
@@ -19,18 +30,33 @@ def link_elements(res):
     return linkElems
 
 
-if len(sys.argv) > 0:
+def open_tabs():
+    """
+        open the links in tabs in the mozilla browser
+    """
+    numOpen = min(5, len(linkElems))
 
     try:
+        for i in range(numOpen):
+            url = 'https://google.com' + linkElems[i].get('href')
+            webbrowser.open_new_tab(url)
+    except:
+        raise ValueError('The url can be opened')
+        pass
+
+
+if len(sys.argv) > 0:
+    """
+        Main part of the program
+
+        sys.argv[1]: search_string
+    """
+    try:
         search_string = sys.argv[1:]
-        res = simple_request(search_string)
+        res = simple_request()
     except:
         print("request error")
         sys.exit(1)
 
     linkElems = link_elements(res)
-    numOpen = min(5, len(linkElems))
-
-    for i in range(numOpen):
-        url = 'https://google.com' + linkElems[i].get('href')
-        webbrowser.open_new_tab(url)
+    open_tabs()
