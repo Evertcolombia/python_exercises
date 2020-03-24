a733fbd36076bb28e2681af7c78660065c6bef86
evertcolombia <evertescalante@gmail.com>
2020-03-24 01:49:19 -0500

    complete randomquiz genertor program

--- 0x08-python_files/randomQuizGenerator.py
+++ 0x08-python_files/randomQuizGenerator.py
@@ -1,37 +1,21 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 
 # RandomQuizGenerator.py - Creates Qyuzzss with questions and answers in random order, along wiht the answer key
 
 import random
 # The quiz data keys are states and values their capitals
 
-capitals = {capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
-   'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
-   'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee',
-   'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise', 'Illinois':
-   'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 'Kansas':
-   'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge', 'Maine':
-   'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 'Michigan':
-   'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson', 'Missouri':
-   'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada':
-   'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 'New
-   Mexico': 'Santa Fe', 'New York': 'Albany',
-   'North Carolina': 'Raleigh', 'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City',
-   'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence',
-   'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee':
-   'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont':
-   'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', 'West
-   Virginia': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
+capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix', 'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver', 'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee', 'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise', 'Illinois': 'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 'Kansas': 'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge', 'Maine': 'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 'Michigan': 'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson', 'Missouri': 'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada': 'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 'New Mexico': 'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh', 'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City', 'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence', 'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee':'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont': 'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', 'West Virginia': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
 
 
 # Generate 35 quiz files
 for quizNum in range (3):
 
     quizFile = open('capitalQuiz%s.txt' % (quizNum + 1), 'w') 
-    answerKeyFile = open('capitalQuiz_answers%s.txt' % (quiznum + 1), 'w')
+    answerKeyFile = open('capitalQuiz_answers%s.txt' % (quizNum + 1), 'w')
 
     quizFile.write('Name:\n\nDate:\n\nPeriod:\n\n')
-    quizFile.write((' ' * 20) + 'State capitals Quiz (form %s)' + % (quizNum + 1))
+    quizFile.write((' ' * 20) + 'State capitals Quiz (form %s)'  % (quizNum + 1))
     quizFile.write('\n\n')
 
     states = list(capitals.keys())
@@ -49,6 +33,12 @@ for quizNum in range (3):
         random.shuffle(answerOptions)
 
         # write the question and the options answer in the quiz file
+        quizFile.write('%s. What is the capital of %s?\n' % (questionNum, states[questionNum]))
+        for i in range(4):
+            quizFile.write('%s. %s\n' % ('ABCD'[i], answerOptions[i]))
+        quizFile.write('\n')
 
         #write the answer key to a file
-
+        answerKeyFile.write('%s. %s\n' % (questionNum + 1, 'ABCD'[answerOptions.index(correctAnswer)]))
+    quizFile.close()
+    answerKeyFile.close()
