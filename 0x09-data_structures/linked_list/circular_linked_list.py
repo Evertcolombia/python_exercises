fd970eb814f6f30e1e3b459684c1fabb16bd13c7
evertcolombia <evertescalante@gmail.com>
2020-08-11 13:39:04 -0500

    adding aplit_linked_list_into_two_halves, josephus_problem, is_circular algorithms to circular linked list

--- 0x09-data_structures/linked_list/circular_linked_list.py
+++ 0x09-data_structures/linked_list/circular_linked_list.py
@@ -41,9 +41,9 @@ class CircularLinkedList:
             if current == self.head:
                 break
 
-    def remove(self, key):
+    def remove(self, node):
         if self.head:
-            if self.head.data == key:
+            if self.head.data == node:
                 current = self.head
                 while current.next != self.head:
                     current = current.next
@@ -58,13 +58,66 @@ class CircularLinkedList:
                 while current.next != self.head:
                     prev = current
                     current = current.next
-                    if current.data == key:
+                    if current.data == node:
                         prev.next = current.next
                         current = current.next
 
+    def __len__(self):
+        """Override len method for this class"""
+        current = self.head
+        count = 0
+
+        while current:
+            count += 1
+            current = current.next
+            if current == self.head:
+                break
+        return count
+
+    def split_list(self):
+        size = len(self)
+
+        if size == 0:
+            return None
+        if size == 1:
+            return self.head
+
+        mid = size // 2
+        count = 0
+
+        prev = None
+        current = self.head
+        while current and count < mid:
+            count += 1
+            prev = current
+            current = current.next
+        prev.next = self.head
+
+        split_cllist = CircularLinkedList()
+        while current.next != self.head:
+            split_cllist.append(current.data)
+            current = current.next
+        split_cllist.append(current.data)
+
+        self.print_list()
+        print()
+        split_cllist.print_list()
+
+    def josephus_circle(self, step):
+        current = self.head
+
+        while len(self) > 1:
+            count = 1
+            while count != step:
+                current = current.next
+                count += 1
+            print('Kill ' + str(current.data))
+            self.remove(current)
+            current = current.next
+
 
 cllist = CircularLinkedList()
-cllist.append("C")
+"""cllist.append("C")
 cllist.append("D")
 cllist.prepend("B")
 cllist.prepend("A")
@@ -72,4 +125,21 @@ cllist.print_list()
 print()
 cllist.remove("A")
 cllist.remove("C")
+cllist.print_list()"""
+
+""""cllist.append("A")
+cllist.append("B")
+cllist.append("C")
+cllist.append("D")
+cllist.append("E")
+cllist.append("F")
+cllist.split_list()"""
+
+cllist.append(1)
+cllist.append(2)
+cllist.append(3)
+cllist.append(4)
+
+
+cllist.josephus_circle(2)
 cllist.print_list()
