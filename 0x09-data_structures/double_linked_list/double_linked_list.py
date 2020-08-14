e1f3e7b29cb097bc90cd4889f87259fdf6162e54
evertcolombia <evertescalante@gmail.com>
2020-08-14 13:43:44 -0500

    adding double linked list algorithms [append_before_node, append_after_node, reverse, delete]

--- 0x09-data_structures/double_linked_list/double_linked_list.py
+++ 0x09-data_structures/double_linked_list/double_linked_list.py
@@ -42,8 +42,76 @@ class DoublyLinkedList:
             print(current.data)
             current = current.next
 
+    def add_after_node(self, key, data):
+        """Add node after node with key"""
+        current = self.head
+        while current:
+            if current.next is None and current.data == key:
+                self.append(data)
+                return
+            elif current.data == key:
+                new_node = Node(data)
+                nxt = current.next
+                current.next = new_node
+                new_node.next = nxt
+                new_node.prev = current
+                nxt.prev = new_node
+                return
+            current = current.next
 
-dllist = DoublyLinkedList()
+    def add_before_node(self, key, data):
+        """Add new node before node with key"""
+        current = self.head
+        while current:
+            if current.prev is None and current.data == key:
+                self.prepend(data)
+                return
+            elif current.data == key:
+                new_node = Node(data)
+                prev = current.prev
+                prev.next = new_node
+                current.prex = new_node
+                new_node.next = current
+                new_node.prev = prev
+                return
+            current = current.next
+
+    def delete(self, key):
+        """Deletes Node from list"""
+        current = self.head
+        while current:
+            if current.data == key and current == self.head:
+                if not current.next:
+                    current = None
+                    self.head = None
+                    return
+                else:
+                    nxt = current.next
+                    current.next = None
+                    nxt.prev = None
+                    current = None
+                    self.head = nxt
+                    return
+            elif current.data == key:
+                if current.next:
+                    nxt = current.next
+                    prev = current.prev
+                    prev.next = nxt
+                    nxt.prev = prev
+                    current.next = None
+                    current.prev = None
+                    current = None
+                    return
+                else:
+                    prev = current.prev
+                    prev.next = None
+                    current.prev = None
+                    current = None
+                    return
+            current = current.next
+
+
+"""dllist = DoublyLinkedList()
 dllist.prepend(0)
 dllist.append(1)
 dllist.append(2)
@@ -51,4 +119,20 @@ dllist.append(3)
 dllist.append(4)
 dllist.prepend(5)
 
+dllist.add_after_node(3,6)
+dllist.add_before_node(4,9)
+
+dllist.print_list()"""
+
+dllist = DoublyLinkedList()
+dllist.append(1)
+dllist.append(2)
+dllist.append(3)
+dllist.append(4)
+
+dllist.delete(1)
+dllist.delete(6)
+dllist.delete(4)
+
+dllist.delete(3)
 dllist.print_list()
