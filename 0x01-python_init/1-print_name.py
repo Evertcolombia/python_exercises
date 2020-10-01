4b535f00b43cf135c79ea096dbd8dcf3702288d3
yugandhartech <67734491+yugandhartech@users.noreply.github.com>
2020-10-01 21:42:49 +0530

    Update 1-print_name.py

--- 0x01-python_init/1-print_name.py
+++ 0x01-python_init/1-print_name.py
@@ -2,8 +2,8 @@
 
 # Ask the user to input their name and assign it to a variable and name
 
-name = input('What is your name ')
+name = input('What is your name ? ')
 
  # Print out hello followed by the name they entered
 
-print('Hello', name)
+print('Hello', name!)
